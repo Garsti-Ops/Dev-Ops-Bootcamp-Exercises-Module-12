42ae8e98aed014e6fd28ae37277323ee83ad3c35
Christoph Heider <christoph.heider@vema-eg.de>
2023-08-30 07:08:19 +0200

    S3 Bucket für Remote-State erstellt und zugewiesen

--- providers.tf
+++ providers.tf
@@ -1,9 +1,16 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "5.13.1"
     }
+  }
+
+  backend "s3" {
+    bucket         = "myapp-s3-bucket"
+    key            = "state/terraform.tfstate"
+    region         = "eu-central-1"
+    encrypt        = true
   }
 }
 
